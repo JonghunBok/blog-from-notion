9353d2174fa4f9599831ffff3e0629b8d7adf28d
Jonghun Park <jonghun.park.194@gmail.com>
2021-07-02 23:52:41 +0900

    first commit

---
+++ app1/src/App.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+//const RemoteButton = React.lazy(() => import("app2/Button"));
+
+const App = () => (
+  <div>
+    <h1>Typescript</h1>
+    <h2>App 1</h2>
+    <p>This is App 1</p>
+  </div>
+);
+
+export default App;
